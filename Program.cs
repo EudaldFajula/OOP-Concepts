19c6e1fa97e0eb3cc2102e3aa8231f85544c24e1
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-01-05 17:31:21 +0100

    Create Inheritance and Polymorphism. Closes #3

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace OppConcepts
     {
         public static void Main()
         {
-            Tree prueba = new Tree(203, "green");
-            Console.WriteLine($"The tree is {prueba.Height} meters tall and the leaves that it has are {prueba.Leaves}");
+            Tree test1 = new Tree(203, "green");
+            Console.WriteLine($"The tree is {test1.Height} meters tall and the leaves that it has are {test1.Leaves}");
+            //Polimorfisme
+            Tree test2 = new PineTree(203, "green", 5);
+            Console.WriteLine($"The pinetree is {test2.Height} meters tall and the leaves that it has are {test2.Leaves} and has {test2.NumPinecone} pinecones");
         }
     }
 }
