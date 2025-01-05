9595857a4a5c725a54c862a9536079b1b50f1ebc
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-01-05 14:52:33 +0100

    Create Instance and builder. Closes #2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using oopconcepts;
+
+namespace OppConcepts
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Tree prueba = new Tree(203, "green");
+            Console.WriteLine($"The tree is {prueba.Height} meters tall and the leaves that it has are {prueba.Leaves}");
+        }
+    }
+}
