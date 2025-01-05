f125d88711ddb15afc8ea336293e09703757cc7e
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-01-05 14:45:05 +0100

    Create object and class. Closes #1

---
+++ Tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopconcepts
+{
+    public class Tree
+    { 
+        public decimal Height { get; set; }
+        public string Leaves { get; set; }
+        public Tree(decimal height, string leaves)
+        {
+            Height = height;
+            Leaves = leaves;
+        }
+    }
+}
