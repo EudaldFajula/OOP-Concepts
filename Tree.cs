3167a2dc7d17d5e6fa5c405130d8fa39474ed438
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-01-05 17:56:57 +0100

    Correct Helper Class

--- Tree.cs
+++ Tree.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace oopconcepts
 {
-    public class Tree
+    public class Tree : ITrees
     { 
         public decimal Height { get; set; }
         public string Leaves { get; set; }
@@ -20,7 +20,7 @@ namespace oopconcepts
         {
             Console.WriteLine("The tree grow up");
         }
-        public static void LeavesFalling()
+        public void LeavesFalling()
         {
             Console.WriteLine("The leaves are falling");
         }
